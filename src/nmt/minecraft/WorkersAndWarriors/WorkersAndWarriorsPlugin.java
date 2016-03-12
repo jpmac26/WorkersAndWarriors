ed532464f7578a333df897d80acabef385819fd8
Skyler Manzanares <skymanzanares@hotmail.com>
2016-03-12 16:39:55 -0700

    Added a session lookup to main plugin

--- src/nmt/minecraft/WorkersAndWarriors/WorkersAndWarriorsPlugin.java
+++ src/nmt/minecraft/WorkersAndWarriors/WorkersAndWarriorsPlugin.java
@@ -6,11 +6,13 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import nmt.minecraft.WorkersAndWarriors.Config.PluginConfiguration;
 import nmt.minecraft.WorkersAndWarriors.Session.GameSession;
+import nmt.minecraft.WorkersAndWarriors.Team.WWPlayer.WWPlayer;
 
 /**
  * Main plugin class. Creates top-level infrastructure, excluding game-session-specific componenets.
@@ -63,5 +65,57 @@ public class WorkersAndWarriorsPlugin extends JavaPlugin implements Listener {
 		for (GameSession session : sessions) {
 			session.stop(true);
 		}
+	}
+	
+	public Set<GameSession> getSessions() {
+		return sessions;
+	}
+	
+	/**
+	 * Searches for a game session with the given name, and returns it. If none is found, null is returned
+	 * instead.
+	 * @param name
+	 * @return
+	 */
+	public GameSession getSession(String name) {
+		if (sessions.isEmpty()) {
+			return null;
+		}
+		
+		for (GameSession session : sessions) {
+			if (session.getName().equals(name)) {
+				return session;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Attempts to find a session the provided player is in.<br />
+	 * Since there's nothing stopping a player form being in multiple sessions, this method returns the
+	 * <i>first</i> session it finds that contains the given player. Order of traversal is undefined.
+	 * @param player
+	 * @return
+	 */
+	public GameSession getSession(WWPlayer player) {
+		return getSession(player.getPlayer());
+	}
+
+	/**
+	 * Attempts to find a session the provided player is in.<br />
+	 * Since there's nothing stopping a player form being in multiple sessions, this method returns the
+	 * <i>first</i> session it finds that contains the given player. Order of traversal is undefined.
+	 * @param player
+	 * @return
+	 */
+	public GameSession getSession(OfflinePlayer player) {
+		for (GameSession session : sessions) {
+			if (session.getPlayer(player) != null) {
+				return session;
+			}
+		}
+		
+		return null;
 	}
 }
