17c5575c795c481b06ef4527ee38e10c66ef4f36
Skyler Manzanares <skymanzanares@hotmail.com>
2016-03-12 00:11:03 -0700

    Updated the flag area class to be functional hehe

--- src/nmt/minecraft/WorkersAndWarriors/Team/FlagArea.java
+++ src/nmt/minecraft/WorkersAndWarriors/Team/FlagArea.java
@@ -1,10 +1,51 @@
 package nmt.minecraft.WorkersAndWarriors.Team;
 
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
 /**
  * Holds information about a flag area, and provides helper functions around it.
  * @author Skyler
  *
  */
 public class FlagArea {
-
+	
+	private Vector min;
+	
+	private Vector max;
+	
+	public FlagArea(Vector point1, Vector point2) {
+		min = new Vector(
+				Math.min(point1.getX(), point2.getX()),
+				Math.min(point1.getY(), point2.getY()),
+				Math.min(point1.getZ(), point2.getZ())
+				);
+		max = new Vector(
+				Math.max(point1.getX(), point2.getX()),
+				Math.max(point1.getY(), point2.getY()),
+				Math.max(point1.getZ(), point2.getZ())				
+				);
+	}
+	
+	/**
+	 * Checks whether a given location is in the bounds of this area.<br />
+	 * Note, however, that <b>the Flag Area doesn't hold world information</b>. This
+	 * is expected to be controlled by either the {@link nmt.minecraft.WorkersAndWarriors.Session.BlockListener BlockListener}
+	 * or the {@link nmt.minecraft.WorkersAndWarriors.Session.GameSession GameSession}
+	 * @param loc Location to check whether it's in the bounds
+	 * @return whether the location is in the bounds
+	 */
+	public boolean isIn(Location loc) {
+		return isIn(loc.toVector());
+	}
+	
+	/**
+	 * Checks whether the given coordinates (presented in a {@link org.bukkit.Vector Vector} object) represent a point within
+	 * the bounds of this flag area.
+	 * @param vect
+	 * @return whether the coordinates lie within this area
+	 */
+	public boolean isIn(Vector vect) {
+		return vect.isInAABB(min, max);
+	}
 }
