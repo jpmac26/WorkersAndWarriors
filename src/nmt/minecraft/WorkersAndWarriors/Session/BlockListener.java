c320d0d7438969be7ebbdc4196617c34b4aa0df3
Skyler Manzanares <skymanzanares@hotmail.com>
2016-03-11 23:27:08 -0700

    added some nice comments

--- src/nmt/minecraft/WorkersAndWarriors/Session/BlockListener.java
+++ src/nmt/minecraft/WorkersAndWarriors/Session/BlockListener.java
@@ -1,5 +1,9 @@
 package nmt.minecraft.WorkersAndWarriors.Session;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
 /**
  * Listens for block changes, and updates the game session.
  * <p>
@@ -12,11 +16,57 @@ package nmt.minecraft.WorkersAndWarriors.Session;
  * @author Skyler
  *
  */
-public class BlockListener {
+public class BlockListener implements Listener {
 
 	private GameSession session;
 	
 	public BlockListener(GameSession session) {
 		this.session = session;
+	}
+	
+	//REMEMBER: make sur a player is in a game before trying to handle anything.
+	//Do this via session.getPlayer() type thing. 
+	
+	/**
+	 * Called when a NON-FLAG block is broken
+	 * @param e
+	 */
+	private void onBlockBreak(BlockBreakEvent e) {
+		//check if player is a worker
+		//if not, don't allow it
+		//if so, make sure it's the opponent's block type
+		//and if it is, allow it. 
+		//otherwise, cancel
+		//e.setCancelled(true);
+	}
+	
+	/**
+	 * Called when a block is placed. NOT A FLAG BLOCK
+	 * @param e
+	 */
+	private void onBlockPlace(BlockPlaceEvent e) {
+		//check if player is a worker AND this is in a valid place
+		//if not, don't allow it
+		//if so, make sure it's the opponent's block type
+		//and if it is, allow it. 
+		//otherwise, cancel
+		//e.setCancelled(true);
+		
+	}
+	
+	/**
+	 * Called when a Flag block is broken
+	 * @param e
+	 */
+	private void onFlagBreak(BlockBreakEvent e) {
+		
+	}
+	
+	/**
+	 * Called when a Flag block is being placed
+	 * @param e
+	 */
+	private void onFlagPlace(BlockPlaceEvent e) {
+		
 	}
 }
