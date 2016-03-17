d1fcbd53d74dc7711841120afa5aef6498dc3b2c
Skyler Manzanares <skymanzanares@hotmail.com>
2016-03-17 11:44:27 -0600

    Filled up player commands!

--- src/nmt/minecraft/WorkersAndWarriors/IO/PlayerCommands.java
+++ src/nmt/minecraft/WorkersAndWarriors/IO/PlayerCommands.java
@@ -9,7 +9,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import nmt.minecraft.WorkersAndWarriors.WorkersAndWarriorsPlugin;
+import nmt.minecraft.WorkersAndWarriors.IO.TeamCommands.SubCommand;
 import nmt.minecraft.WorkersAndWarriors.Session.GameSession;
+import nmt.minecraft.WorkersAndWarriors.Team.Team;
 import nmt.minecraft.WorkersAndWarriors.Team.WWPlayer.WWPlayer;
 
 public class PlayerCommands implements CommandExecutor {
@@ -154,10 +156,48 @@ public class PlayerCommands implements CommandExecutor {
 	}
 	
 	private boolean teamCommand(Player sender, String[] args) {
+		//wwp team [session] [team]
+		if (args.length != 3) {
+			sender.sendMessage(ChatFormat.USAGE + "Usage: /wwtp " + SubCommand.TEAM.getName() 
+					+ ChatFormat.SESSION + " [session] " + ChatFormat.TEAM.wrap("[team]"));
+			return true;
+		}
+		
+		GameSession session = WorkersAndWarriorsPlugin.plugin.getSession(args[1]);
+		if (session == null) {
+			sender.sendMessage(ChatFormat.ERROR + "Unable to locate session " + ChatFormat.SESSION.wrap(args[1]));
+			sender.sendMessage(ChatFormat.USAGE + "Usage: /wwp " + SubCommand.TEAM.getName() 
+					+ ChatFormat.SESSION + " [session] " + ChatFormat.TEAM.wrap("[team]"));
+			return true;			
+		}
+		
+		WWPlayer wp = session.getPlayer(sender);
+		if (wp == null) {
+			sender.sendMessage(ChatFormat.WARNING.wrap("You must join the session first!"));
+			sender.sendMessage(ChatFormat.INFO.wrap("To join, use /wwp join ") 
+					+ ChatFormat.SESSION.wrap(args[1]));
+			return true;
+		}
+		
+		Team team = session.getTeam(args[2]);
+		if (team == null) {
+			sender.sendMessage(ChatFormat.ERROR.wrap("Could not find team ")
+					+ ChatFormat.TEAM.wrap(args[2]));
+			sender.sendMessage(ChatFormat.USAGE + "Usage: /wwp " + SubCommand.TEAM.getName() 
+					+ ChatFormat.SESSION + " [session] " + ChatFormat.TEAM.wrap("[team]"));
+			return true;
+		}
+		
+		team.addPlayer(wp);
+		sender.sendMessage(ChatFormat.SUCCESS.wrap("You have joined the team ") 
+				+ ChatFormat.TEAM.wrap(team.getTeamName()));
+		
+		
 		return true;
 	}
 	
 	private boolean menuCommand(Player sender, String[] args) {
+		sender.sendMessage(ChatFormat.INFO.wrap("Not yet implemented!"));
 		return true;
 	}
 
