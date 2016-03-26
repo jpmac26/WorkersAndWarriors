00da177a35c03a813c5b64e37f356018ebe3040e
Trigition <trigition@gmail.com>
2016-03-25 22:35:47 -0600

    Added more resets on playerdeath

--- src/nmt/minecraft/WorkersAndWarriors/Session/Respawn.java
+++ src/nmt/minecraft/WorkersAndWarriors/Session/Respawn.java
@@ -61,6 +61,9 @@ public class Respawn implements Tickable<Object>{
 		p.teleport(spawnLocation);
 		p.setGameMode(GameMode.SURVIVAL);
 		p.setHealth(p.getMaxHealth());
+		p.setExhaustion(0.0f);
+		p.setSaturation(15.0f);
+		p.setFoodLevel(20);
 		p.setTicksLived(0);
 		p.getActivePotionEffects().clear();
 		// Apply potion effects
